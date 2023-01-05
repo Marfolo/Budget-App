5520501bb5213401733cf96db8e1e168ae1f0d1a
Marfolo <matti0@onet.pl>
2023-01-05 11:26:45 +0100

    More code

--- budget.py
+++ budget.py
@@ -5,7 +5,14 @@ class Category:
 	def __init__(self, category_name):
 		self.category = category_name
 
+	def deposit(self, amount, description):
+		if description is None:
+			description == ""
+		self.ledger.append({"amount": amount, "description": description})
 
+	def withdraw(self, amount, description):
+		#self.ledger.append({"amount": -amount, "description": description})
+		if amount > ledger.
 
 
 def create_spend_chart(categories):
