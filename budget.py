01019c7fc1fdeff44be461a3b06cf4ea85cc0604
Marfolo <matti0@onet.pl>
2023-01-15 17:29:34 +0100

    Code

--- budget.py
+++ budget.py
@@ -12,7 +12,28 @@ class Category:
 
 	def withdraw(self, amount, description):
 		#self.ledger.append({"amount": -amount, "description": description})
-		if amount > ledger.
+		if (self.check_funds(amount)):
+			self.ledger.append({"amount": -amount, "description": description})
+			return True
+		return False
 
+	def get_balance(self):
+		money = 0
+		for item in self.ledger:
+			money += item["amount"]
+		return money
+
+	def transfer(self, amount, category):
+		if (self.check_funds(amount)):
+			self.withdraw(amount, "Transfer to " + category.category)
+			category.deposit(amount, "Transfer from " + self.category)
+			return True
+		return False
+
+	def check_funds(self, amount):
+		if (self.get_balance() >= amount):
+			return True
+		return False
 
 def create_spend_chart(categories):
+	return None
