058b29d92fc57755f5b0d9ddd3a4767ef88d6a18
Marfolo <matti0@onet.pl>
2023-01-16 16:48:43 +0100

    Copied from Londan Schlangen

--- budget.py
+++ budget.py
@@ -1,3 +1,54 @@
+def truncate(n):
+	multiplier = 10
+	return int(n * multiplier) / multiplier
+
+def getTotals(categories):
+	total = 0
+	breakdown = []
+	for category in categories:
+		total += category.get_withdrawls()
+		breakdown.append(category.get_withdrawls())
+	rounded = list(map(lambda x: truncate(x/total), breakdown))
+	return rounded
+
+def create_spend_chart(categories):
+	res = "Percentage spend by category\n"
+	i = 100
+	totals = getTotals(categories)
+	while i >= 0:
+		cat_spaces = " "
+		for total in totals:
+			if total * 100 >= i:
+				cat_spaces += "o  "
+			else:
+				cat_spaces += "   "
+		res += str(i).rjust(3) + "|" + cat_spaces + ("\n")
+		i -= 10
+
+	dashes = "-" + "---" * len(categories)
+	names = []
+	x_axis = ""
+	for category in categories:
+		names.append(category.category)
+
+	maxi = max(names, key=len)
+
+	for x in range(len(maxi)):
+		nameStr = '     '
+		for name in names:
+			if x >= len(name):
+				nameStr += "   "
+			else:
+				nameStr += name[x] + "  "
+
+		if (x != len(maxi) -1 ):
+			nameStr += '\n'
+
+		x_axis += nameStr
+
+	res += dashes.rjust(len(dashes) + 4) + '\n' + x_axis
+	return res
+
 class Category:
 	ledger = []
 	category = ""
@@ -43,5 +94,9 @@ class Category:
 			return True
 		return False
 
-def create_spend_chart(categories):
-	return None
+	def get_withdrawls(self):
+		total = 0
+		for item in self.ledger:
+			if item['amount'] < 0:
+				total += item['amount']
+		return total
